dd46f64f889ca453949e2173b8521c094de7bf92
Tai Yik Hui <yik-hui.tai1@phelma.grenoble-inp.fr>
2016-01-28 17:28:34 +0100

    ajout sysout pour tester

--- Tp_SD/src/com/tp/consensus/Process.java
+++ Tp_SD/src/com/tp/consensus/Process.java
@@ -10,108 +10,144 @@ public class Process {
 	private boolean received;
 	private int compteur;
 	private Process proposer;
-	private LinkedList<Message> pending = new LinkedList<Message> ();
-	private LinkedList<Message> delivered = new LinkedList<Message> ();
+	private LinkedList<Message> pending = new LinkedList<Message>();
+	private LinkedList<Message> delivered = new LinkedList<Message>();
 	private int neighboursNum;
-	private LinkedList<Process> neighbours =new LinkedList<Process>();
-	public Process(int processId){
-		this.processId=processId;
-		this.alive=true;
+	private LinkedList<Process> neighbours = new LinkedList<Process>();
+
+	public Process(int processId) {
+		this.processId = processId;
+		this.alive = true;
 	}
-	public void receiveMsg(Message message){
-		if (isAlive()){
-			getPending().add(message);
-		}
-	}
-	public void propose(Message message){
-		if(isAlive()){
-			for (Process n:neighbours){
-				n.setReceived(message);
-				n.setProposer(this);
-			}
-			setLeader(true);		
-		}
-	}
-	public void acknowledge(Process proposer, Message message){
-		if (isAlive()){
-			getPending().add(message);
-			proposer.setCompteur(getCompteur()+1);
-		}
-	}
-	public void deliver(Message message){
-		if (isAlive()){
-			getPending().remove(message);
-			getDelivered().add(message);
-			setDeliver(true);
-		}
-	}
-	public void deliver(Process proposer, Message message){
-		if(proposer.isDeliver() && isAlive()){
-			getPending().remove(message);
-			getDelivered().add(message);
-		}
-	}
-	public int getNeighboursNum(){
+
+	public int getNeighboursNum() {
 		return this.neighboursNum;
 	}
-	public void setNeighboursNum(int neighboursNum){
-		this.neighboursNum=neighboursNum;
+
+	public void setNeighboursNum(int neighboursNum) {
+		this.neighboursNum = neighboursNum;
 	}
+
 	public int getProcessId() {
 		return this.processId;
 	}
-	public void setProcessId(int processId){
-		this.processId=processId;
+
+	public void setProcessId(int processId) {
+		this.processId = processId;
 	}
+
 	public LinkedList<Process> getNeighbours() {
 		return this.neighbours;
 	}
-	public void setNeighbours(LinkedList<Process> neighbours){
-		this.neighbours=neighbours;
+
+	public void setNeighbours(LinkedList<Process> neighbours) {
+		this.neighbours = neighbours;
 	}
-	public LinkedList<Message> getPending(){
+
+	public LinkedList<Message> getPending() {
 		return this.pending;
 	}
-	public void setPending(LinkedList<Message> mess){
+
+	public void setPending(LinkedList<Message> mess) {
 		this.pending = mess;
 	}
-	public LinkedList<Message> getDelivered(){
+
+	public LinkedList<Message> getDelivered() {
 		return this.delivered;
 	}
+
 	public boolean isAlive() {
 		return alive;
 	}
+
 	public void setAlive(boolean alive) {
 		this.alive = alive;
 	}
+
 	public boolean isLeader() {
 		return leader;
 	}
+
 	public void setLeader(boolean leader) {
 		this.leader = leader;
 	}
+
 	public boolean isReceived(Message received) {
 		return this.received;
 	}
-	public void setReceived(Message received) {
-		this.received=true;
+
+	public void receivedLeaderMessage(Message received) {
+		this.received = true;
+		//getPending().add(received);
 	}
+	
+	public void setReceived(boolean received) {
+		this.received = received;
+	}
+
 	public int getCompteur() {
 		return compteur;
 	}
+
 	public void setCompteur(int compteur) {
 		this.compteur = compteur;
 	}
+
 	public boolean isDeliver() {
 		return deliver;
 	}
+
 	public void setDeliver(boolean deliver) {
 		this.deliver = deliver;
 	}
+
 	public Process getProposer() {
 		return proposer;
 	}
+
 	public void setProposer(Process proposer) {
 		this.proposer = proposer;
+	}
+	
+	public void receiveClientMsg(Message message) {
+		if (isAlive()) {
+			getPending().add(message);
+		}
+	}
+
+	public void propose(Message message) { // send msg to neighbours = call
+											// their ack
+		if (isAlive()) {
+			for (Process n : neighbours) {
+				n.receivedLeaderMessage(message);
+				n.setProposer(this);
+			}
+			System.out.println("Proc " + this.processId + " broadcast message " + message.getId());
+		}
+	}
+
+	public void acknowledge(Process proposer, Message message) {
+		if (isAlive()) {
+			getPending().add(message);
+			proposer.setCompteur(getCompteur() + 1);
+			System.out.println("Proc " + this.processId + " acknowledges message " + message.getId());
+		}
+	}
+
+	public void deliver(Message message) {
+		if (isAlive()) {
+			getPending().remove(message);
+			getDelivered().add(message);
+			setDeliver(true);
+			System.out.println("Proc " + this.processId + "(leader) delivers message " + message.getId());
+		}
+	}
+
+	public void deliver(Process proposer, Message message) {
+		if (proposer.isDeliver() && isAlive()) {
+			getPending().remove(message);
+			getDelivered().add(message);
+			System.out.println("Proc " + this.processId + "(acceptor) delivers message " + message.getId());
+		}
 	}
 }
--- Tp_SD/src/com/tp/consensus/TProcess.java
+++ Tp_SD/src/com/tp/consensus/TProcess.java
@@ -6,51 +6,56 @@ public class TProcess implements Runnable {
 	private Process p;
 	private String name;
 	private int minGlobal;
-	
-	public TProcess(Process p, String name, int minGlobal){
+
+	public TProcess(Process p, String name, int minGlobal) {
 		this.p = p;
 		this.name = name;
 		this.minGlobal = minGlobal;
 	}
-	
-	public static Message minimum (LinkedList<Message> l){
+
+	public static Message minimum(LinkedList<Message> l) {
 		Message min = l.getFirst();
 		int minId = l.getFirst().getId();
-		for(int cpt = 2; cpt <= l.size(); cpt++){
-			if (minId > l.get(cpt-1).getId()){
-				min = l.get(cpt-1);
-				minId = l.get(cpt-1).getId();
+		for (int cpt = 2; cpt <= l.size(); cpt++) {
+			if (minId > l.get(cpt - 1).getId()) {
+				min = l.get(cpt - 1);
+				minId = l.get(cpt - 1).getId();
 			}
 		}
 		return min;
 	}
-	
-	public void init(){
+
+	public void init() {
 		LinkedList<Message> pending = MessageSerializerDeserializer.getInstance().messageDeserializer(this.name);
 		// un proc qui revit..
-		if(pending != null){
+		if (pending != null) {
 			p.setPending(pending);
 		}
 	}
-	
+
 	public void run() {
 		init();
-		while(true){
-			if(!p.getPending().isEmpty()){
-				Message min = minimum (p.getPending());
-				if (min.getId() == minGlobal) p.propose(min);
-				if (!p.isLeader() && p.isAlive() && p.isReceived(min)){
-					p.acknowledge(p.getProposer(),min);
+		while (true) {
+			if (!p.getPending().isEmpty()) {
+				Message min = minimum(p.getPending());
+				if (min.getId() == minGlobal) {
+					p.setLeader(true);
+					p.propose(min);
 				}
-				else if (p.isLeader() && p.isAlive()) {
-					while (p.getCompteur() <= p.getNeighbours().size()/2 );
+				if (!p.isLeader() && p.isAlive() && p.isReceived(min)) {
+					//min = minimum(p.getPending()); probleme car min != message recu de leader..
+					p.acknowledge(p.getProposer(), min);
+				} else if (p.isLeader() && p.isAlive()) {
+					while (p.getCompteur() <= p.getNeighbours().size() / 2)
+						;
 					p.deliver(min);
 				}
-				if (!p.isLeader() && p.isAlive()&& p.isReceived(min)){
-					p.deliver(p.getProposer(),min);
+				if (!p.isLeader() && p.isAlive() && p.isReceived(min)) {
+					p.deliver(p.getProposer(), min);
 				}
 				p.setLeader(false);
 				p.setDeliver(false);
+				p.setReceived(false);
 			}
 
 		}
